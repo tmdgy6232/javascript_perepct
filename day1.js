593d3d33229f500a978b79651c25580f4633def7
seunghyo <tmdgy6232@naver.com>
2021-12-31 22:11:11 +0900

    [1일] function 클래스 사용 및 prototype 사용

--- day1.js
+++ day1.js
@@ -6,5 +6,22 @@ arr1.plus = function () {
     return t1 + t2
 }
 
+//객체를 초기화하기 위해 생성자 함수 정의
+// 약간 파이썬 생성자 느낌인듯.
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
+// new Keyword로 객체를 생성할 때 앞에서 정의한 생성자 함수 사용
+var p = new Point(1, 1) // 2차원 좌표 (1,1)
 
+// 생성자 함수 Point의 prototype 객체에 함수를 정의함으로서
+// Point 객체에 메서드를 정의한다.
+
+Point.prototype.r = function () {
+    return Math.sqrt(this.x * this.x + this.y * this.y) // this는 이 메서드가 호출된 Point 객체를 가리킨다.
+}
+
+// Point 객체 p 뿐만 아니라 모든 Point의 객체는 메서드 r을 상속받는다.
+console.log(p.r())
