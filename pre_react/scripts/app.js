43bd4d38a2df507d90515175f11d7f33c80486b9
seunghyo <tmdgy6232@naver.com>
2023-12-03 18:56:35 +0900

    create: js강의코드 추가 import, export

---
+++ pre_react/scripts/app.js
@@ -0,0 +1,5 @@
+import * as util from "./utils";
+import test1 from "./utils";
+console.log(util.apiKey);
+console.log(util.test);
+console.log(test1);
---
+++ pre_react/scripts/utils.js
@@ -0,0 +1,3 @@
+export let apiKey = "asdlkjflaksdjf";
+export default "default Export is only value and just one default created";
+export let test = "test";
