88c191f9c381f0a1e17372f04d4e0b04f06e8acd
seunghyo <tmdgy6232@naver.com>
2023-12-07 11:06:12 +0900

    create: react Props

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month:'long'});
+    const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+    const year = props.date.getFullYear();
 
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'car Insurance';
-    const expenseAmount = 294.67;
+    
     return (
         <div className="expense-item">
-            <div>{expenseDate.toString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
