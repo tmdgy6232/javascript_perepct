1e17e51cc5462049d4e65c3b2f1f7c9fb5aaad55
seunghyo <tmdgy6232@naver.com>
2023-12-11 14:36:20 +0900

    create: React JSX 동작방법 상세

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses";
-
+import React from 'react';
 function App() {
   const expenses = [
     {
@@ -22,6 +22,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement('div',
+  // {},
+  // React.createElement('h2', {}, "Let's get started!"),
+  // React.createElement(Expenses, {items : expenses}));
   return (
     <div>
       <h2>Let's get started!</h2>
