1381ead65239dc44a3b09005def9d2b32c9f3c4e
seunghyo <tmdgy6232@naver.com>
2023-12-13 21:45:33 +0900

    React: 공유핸들러 함수 사용

--- 01-starting-setup/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
   const titleChangeHandler = (event) => {
-    //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
   };
   const amountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
+    setEnteredAmount(event.target.value);
     // 객체를 활용할 때 이 방법도 통하긴 하지만 이전 상태를 의존하게 될 위험이 있음.
     // 기본적으로 상태변경을 '예약'하기 때문임.
 
@@ -30,24 +30,37 @@ const ExpenseForm = () => {
 
     // 그러므로 아래와 같이 셋팅함수 내부에서 함수를 즉시 실행시켜 변환시키는 방법이 더 좋음.
     // 이 방법은 예약된 모든 상태 업데이트를 기억하고 이전 상태를 항상 최신상태로 리액트가 유지시켜줌
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAmount: event.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
   };
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
-
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value);
+    } else if (identifier === "date") {
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={(event) => {
+              inputChangeHandler("title", event.target.value);
+            }}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -55,7 +68,9 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={amountChangeHandler}
+            onChange={(event) => {
+              inputChangeHandler("amount", event.target.value);
+            }}
           />
         </div>
         <div className="new-expense__control">
@@ -64,7 +79,9 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             nax="2023-12-31"
-            onChange={dateChangeHandler}
+            onChange={(event) => {
+              inputChangeHandler("date", event.target.value);
+            }}
           />
         </div>
       </div>
