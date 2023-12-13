e007b100533179950d82531360940790f69c3b23
seunghyo <tmdgy6232@naver.com>
2023-12-13 23:49:10 +0900

    React: 파생/계산된 상태 예제

--- 01-starting-setup/01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses/Expenses.js
@@ -6,6 +6,18 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2020");
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filterYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filterYear === "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else if (filterYear === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else {
+    filterInfoText = "2019, 2020 & 2021";
+  }
+
   const filterChangeHandler = (selectYear) => {
     setFilterYear(selectYear);
   };
@@ -17,6 +29,9 @@ const Expenses = (props) => {
           initYear={filterYear}
           onChangeFilter={filterChangeHandler}
         />
+        <p style={{ color: "white" }}>
+          Data for years {filterInfoText} is hidden.
+        </p>
         {props.items.map((item) => (
           <ExpenseItem
             key={item.id}
