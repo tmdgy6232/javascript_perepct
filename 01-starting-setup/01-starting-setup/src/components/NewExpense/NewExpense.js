1038f8f2e97e73a742c9da2c545a5ca0cb486544
seunghyo <tmdgy6232@naver.com>
2023-12-15 23:08:31 +0900

    React: Toggle Button

--- 01-starting-setup/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={props.onSaveExpenseData} />
+      {!isEditing ? (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      ) : (
+        <ExpenseForm onSaveExpenseData={props.onSaveExpenseData} />
+      )}
     </div>
   );
 };
