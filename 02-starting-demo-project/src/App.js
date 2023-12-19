41b91a56b82cfeb9e6e297929fec94c4bbf62486
seunghyo <tmdgy6232@naver.com>
2023-12-19 14:24:15 +0900

    React: managed state code in app.js

--- 02-starting-demo-project/src/App.js
+++ 02-starting-demo-project/src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./compononts/Users/AddUser";
+import UsersList from "./compononts/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prev) => {
+      return [
+        ...prev,
+        { name: uName, age: +uAge, key: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
