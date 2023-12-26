828c1d8152e741b6fa3f7975d9fd930444efd329
seunghyo <tmdgy6232@naver.com>
2023-12-26 21:13:50 +0900

    React: 8th optimization React Demo app!

---
+++ 08-optimization/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prev) => {
+      return !prev;
+    });
+  };
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
