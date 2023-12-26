6dec2006b0725ac6116c4bda8ea2aaacbb0b4112
seunghyo <tmdgy6232@naver.com>
2023-12-26 21:42:32 +0900

    React: 8th optimization App React.memo를 통한 최적화와 대부분 모르고 지나가는 버그(참조값 비교)

--- 08-optimization/src/App.js
+++ 08-optimization/src/App.js
@@ -15,10 +15,13 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
-
+/**
+ * App.js가 재실행 될때마다 함수를 만들고, 함수는 주소값이 계속 바뀌므로 Button을 React.memo로 감싸 최적화를 하더라도 props가
+ * 변화했다고 인지하기 때문에 Button Component는 재평가된다.
+ */
 export default App;
--- 08-optimization/src/components/Demo/DemoOutput.js
+++ 08-optimization/src/components/Demo/DemoOutput.js
@@ -1,7 +1,13 @@
 import React from "react";
+import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
-  return <p>{props.show ? "This is new!" : ""}</p>;
+  console.log("demooutput running");
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+/**
+ * React.memo 함수는 component에 전달하는 Props를 기억해뒀따가
+ * props가 변경되었을 경우에만 재실행 및 재평가 한다.
+ */
+export default React.memo(DemoOutput);
