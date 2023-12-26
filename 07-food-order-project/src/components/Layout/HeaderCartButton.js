21e63840898cb2efba1faf57b7faab136ade02f1
seunghyo <tmdgy6232@naver.com>
2023-12-26 15:21:43 +0900

    React: 7th demo app CartReducer 추가

--- 07-food-order-project/src/components/Layout/HeaderCartButton.js
+++ 07-food-order-project/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,20 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import clasees from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={clasees.button} onClick={props.onShowCart}>
       <span className={clasees.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={clasees.badge}>3</span>
+      <span className={clasees.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
