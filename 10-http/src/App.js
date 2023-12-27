438b694dbda1f87d67db5a94a412d95e73c2a1f8
seunghyo <tmdgy6232@naver.com>
2023-12-27 18:46:03 +0900

    React: Http 로딩 및 데이터 상태 처리하기

--- 10-http/src/App.js
+++ 10-http/src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchMovieHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
   // const dummyMovies = [
   //   {
@@ -40,7 +43,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading....</p>}
       </section>
     </React.Fragment>
   );
