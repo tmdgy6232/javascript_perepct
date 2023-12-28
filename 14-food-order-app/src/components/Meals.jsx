f26bc16b058797959f1e4aa1245a056fd40543b4
seunghyo <tmdgy6232@naver.com>
2023-12-29 00:27:02 +0900

    React: 14th demo app useHttp custom Hook을 이용한 백엔드 통신

--- 14-food-order-app/src/components/Meals.jsx
+++ 14-food-order-app/src/components/Meals.jsx
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+// 객체 생성하면 useEffect때문에 무한루프 도니까 밖에다가 생성
+const requestConfig = {};
 export default function Meals() {
-  const [loadedmeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedmeals,
+    error,
+    isLoading,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-
-      if (!response.ok) {
-        // ...
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching ...</p>;
+  }
 
   return (
     <ul id="meals">
---
+++ 14-food-order-app/src/hooks/useHttp.js
@@ -0,0 +1,43 @@
+import { useEffect, useState, useCallback } from "react";
+
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something wnet wrong, failed to send request."
+    );
+  }
+  return resData;
+}
+
+export default function useHttp(url, config, initialData) {
+  const [data, setData] = useState(initialData);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const sendRequestFunc = useCallback(
+    async function sendRequest() {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, config);
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    },
+    [url, config]
+  );
+  useEffect(() => {
+    if ((config && (config.method === "GET" || !config.method)) || !config) {
+      sendRequestFunc();
+    }
+  }, [sendRequestFunc, config]);
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequestFunc,
+  };
+}
