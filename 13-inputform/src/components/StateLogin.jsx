64be4b611af27008445f05b8d84a67c0ffd17afc
seunghyo <tmdgy6232@naver.com>
2023-12-28 13:02:26 +0900

    React: inputForm Blur 상태로 validation 하기

--- 13-inputform/src/components/StateLogin.jsx
+++ 13-inputform/src/components/StateLogin.jsx
@@ -9,6 +9,11 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -17,6 +22,17 @@ export default function Login() {
   function handleInputChange(identifier, value) {
     // javascript object key 동적입력
     setEnteredValues((prev) => ({ ...prev, [identifier]: value }));
+    setDidEdit((prev) => ({
+      ...prev,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
   }
   // function handleEmailChange(event) {
   //   setEnteredEmail(event.target.value);
@@ -37,8 +53,12 @@ export default function Login() {
             type="email"
             name="email"
             value={enteredValues.email}
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -48,6 +68,7 @@ export default function Login() {
             type="password"
             name="password"
             value={enteredValues.password}
+            onBlur={() => handleInputBlur("password")}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
