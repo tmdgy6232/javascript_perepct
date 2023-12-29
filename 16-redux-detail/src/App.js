8b710ee5743836c7057a19de09ba4f832eab96a9
seunghyo <tmdgy6232@naver.com>
2023-12-29 22:37:25 +0900

    React: React Thunk, Thunk Action Creator 사용해보기(useEffect 대안)

--- 16-redux-detail/src/App.js
+++ 16-redux-detail/src/App.js
@@ -5,7 +5,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,48 +15,12 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.setNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-
-      const response = await fetch(
-        "https://react-http-c87b4-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart Data failed.");
-      }
-      dispatch(
-        uiActions.setNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.setNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <>
