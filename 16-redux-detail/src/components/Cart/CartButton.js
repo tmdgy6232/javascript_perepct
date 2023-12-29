f069cbaf0eaa906c8083036fc1d49addcb4e9497
seunghyo <tmdgy6232@naver.com>
2023-12-29 22:14:23 +0900

    React: 16th 비동기 함수 useEffect를 통해 redux와 함꼐 사용하기

--- 16-redux-detail/src/components/Cart/CartButton.js
+++ 16-redux-detail/src/components/Cart/CartButton.js
@@ -1,10 +1,16 @@
-import classes from './CartButton.module.css';
-
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch, useSelector } from "react-redux";
 const CartButton = (props) => {
+  const dispath = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  const toggleCartHandler = () => {
+    dispath(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity} </span>
     </button>
   );
 };
