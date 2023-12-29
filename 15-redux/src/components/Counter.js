208aa14ceaa819499ddd506ca3a1d91d94572f0b
seunghyo <tmdgy6232@naver.com>
2023-12-29 11:45:51 +0900

    React: 15th Redux 컴포넌트에 연결하여 사용하기

--- 15-redux/src/components/Counter.js
+++ 15-redux/src/components/Counter.js
@@ -1,12 +1,26 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux"; // 리액트 리덕스에서 만든 커스텀 훅
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementhandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementhandler = () => {
+    dispatch({ type: "decrement" });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementhandler}>Increment</button>
+        <button onClick={decrementhandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
