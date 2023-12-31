e5fd32c6b2ebb04071a9477fa75d55bcc412f4fb
seunghyo <tmdgy6232@naver.com>
2023-12-31 10:48:25 +0900

    React: React Router 페이지 이동하기 feat.Link Component

--- 17-react-route/src/App.js
+++ 17-react-route/src/App.js
@@ -1,25 +1,25 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  //createRoutesFromElements,
+  // Route,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPage />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPage /> },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+//const router = createBrowserRouter(routeDefinitions);
 function App() {
   return <RouterProvider router={router} />;
 }
