ab3e5432e2b3a91989c9cdff1644bfdd991684a2
seunghyo <tmdgy6232@naver.com>
2023-12-31 10:40:46 +0900

    React: Route 정의하기 version2

--- 17-react-route/src/App.js
+++ 17-react-route/src/App.js
@@ -1,6 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+import ProductPage from "./pages/Products";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductPage /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 function App() {
   return <RouterProvider router={router} />;
 }
