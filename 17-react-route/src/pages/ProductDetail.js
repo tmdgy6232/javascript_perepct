490b7adf23406221e60dd1d67816c4c8db3f665c
seunghyo <tmdgy6232@naver.com>
2024-01-01 16:52:58 +0900

    React: Router 절대, 상대경로, path, router 기준 link Relative props 사용

--- 17-react-route/src/pages/ProductDetail.js
+++ 17-react-route/src/pages/ProductDetail.js
@@ -1,11 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 function ProductDetailPage() {
   const params = useParams();
   const data = params.productId;
+  /**
+   * relative는 패스를 기준으로 할지 라우트를 기준으로 할지 정해준다.
+   * ..는 상위 한단계 올라가는 개념이므로
+   * 라우트 기준으로 하면 홈페이지로 가고, 패스 기준으로 가면 패스만 한단계 올려준다.
+   */
   return (
     <>
       <h1>Product Details!</h1>
       <p>{params.productId}</p>
+      <a>
+        <Link to=".." relative="path">
+          back!
+        </Link>
+      </a>
     </>
   );
 }
