3e6f52c6d8bb4c1d2db7249fa18cdfca288439ea
seunghyo <tmdgy6232@naver.com>
2024-01-01 16:31:33 +0900

    React: 동적라우터 링크 추가하기

--- 17-react-route/src/pages/Products.js
+++ 17-react-route/src/pages/Products.js
@@ -1,11 +1,28 @@
+import { Link } from "react-router-dom";
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product1",
+  },
+  {
+    id: "p2",
+    title: "Product2",
+  },
+  {
+    id: "p3",
+    title: "Product3",
+  },
+];
 function ProductPage() {
   return (
     <>
       <h1>Product Page</h1>
       <ul>
-        <li>Product1</li>
-        <li>Product2</li>
-        <li>Product3</li>
+        {PRODUCTS.map((item) => (
+          <li key={item.id}>
+            <Link to={`/products/${item.id}`}>{item.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
