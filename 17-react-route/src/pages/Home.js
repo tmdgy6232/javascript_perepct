ddb6fa60706b406b36e50ccfa9954745850a863f
seunghyo <tmdgy6232@naver.com>
2024-01-01 15:55:11 +0900

    React: router 프로그램적으로 페이지 이동하기 feat useNavigate

--- 17-react-route/src/pages/Home.js
+++ 17-react-route/src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My home page</h1>
       <p>
         Go to the <Link to="/products">list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Naviagte</button>
       </p>
     </>
   );
