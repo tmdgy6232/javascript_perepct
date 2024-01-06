5b79d590179bbd692b856a7eceb886dc62dc307c
seunghyo <tmdgy6232@naver.com>
2024-01-06 19:10:23 +0900

    React: Loader함수 Response 객체 리턴

--- 18-react-route-detail/frontend/src/pages/Events.js
+++ 18-react-route-detail/frontend/src/pages/Events.js
@@ -14,6 +14,9 @@ export async function loader() {
   if (!response.ok) {
     // ...
   } else {
+    // useLoaderData는 fetch가 Promise<Response> 타입을 리턴하기때문에
+    // 그에맞게 Response 객체 자체를 전달해도 안에 데이터를 추출해서 전달해준다.
+    // 그러므로 추출 작업 안하고 response 자체를 리턴해도 작동한다.
     const resData = await response.json();
     return resData.events;
   }
