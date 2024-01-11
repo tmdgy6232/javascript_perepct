33c991a009b7cc47a2d0b0950ee0ecf4f0c101dc
seunghyo <tmdgy6232@naver.com>
2024-01-11 11:20:53 +0900

    React: useNavigation을 이용한 form 제출 시 ui 변경

--- 18-react-route-detail/frontend/src/components/EventForm.js
+++ 18-react-route-detail/frontend/src/components/EventForm.js
@@ -1,9 +1,14 @@
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  // router가 데이터를 제출하고 있다면 useNavigation hook에서 state가 submitting인지 확인해보면 알 수 있따.
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
@@ -54,10 +59,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
