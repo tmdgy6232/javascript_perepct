f9b88702bd586112ebef2c26829a1175a2f9baec
seunghyo <tmdgy6232@naver.com>
2024-01-17 18:15:26 +0900

    React: 토큰만료 관리하기

--- 19-authenticate/frontend/src/pages/Root.js
+++ 19-authenticate/frontend/src/pages/Root.js
@@ -1,10 +1,29 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
 
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
   return (
     <>
       <MainNavigation />
