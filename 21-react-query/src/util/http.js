9ea0d4e26389e1cada13c48f34aee85ddaefa67e
seunghyo <tmdgy6232@naver.com>
2024-03-13 23:19:02 +0900

    react: reactQuery 동적 쿼리 구성 객체 및 요청 취소 예쩨

--- 21-react-query/src/util/http.js
+++ 21-react-query/src/util/http.js
@@ -1,5 +1,10 @@
-export async function fetchEvents() {
-  const response = await fetch("http://localhost:3000/events");
+// 리액트 데이터 가져오는중에 사용자가 페이지를 나가거나 하면 signal을 통해 요청을 취소할 수 있따.
+export async function fetchEvents({ signal, searchTerm }) {
+  let url = "http://localhost:3000/events";
+  if (searchTerm) {
+    url += "?search=" + searchTerm;
+  }
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
